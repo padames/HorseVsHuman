9c41e8314100e327c241408e75f9aac6861b0136
Pablo Adames <pablo.adames@gmail.com>
2020-06-20 00:52:32 -0600

    main script calls split-data script correctly

--- horses-humans.R
+++ horses-humans.R
@@ -18,7 +18,6 @@ rm(data.dir, horses.dir)
 # let's separate the data into train, validation and test
 data.horses.dir <- file.path(cur.dir, "data", "all-horses")
 
-source("split.data.R")
 if (checkDirectoryExists(data.horses.dir) == TRUE) {
   f.lst <- list.files(data.horses.dir) 
   if (length(f.lst) < 628)
@@ -27,9 +26,19 @@ if (checkDirectoryExists(data.horses.dir) == TRUE) {
 
 # split the data into train, validation and test
 
+cur.dir <- getwd()
+train.horses.dir <- file.path(cur.dir, "data", "train", "horses")
+
+# do a check to see if files have been aggregated, otherwise call the script that does it
+if (checkDirectoryExists(train.horses.dir) == TRUE) {
+  f.lst <- list.files(train.horses.dir) 
+  if (length(f.lst) < 439)
+    source("split-files.R")
+}
 
 
-# use the image generator to label the data
+# building the network
+
 
 
 
